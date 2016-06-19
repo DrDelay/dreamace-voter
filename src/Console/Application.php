3fb1f366f8c4ddca9db23bbd874be9d7dc73aa25
DrDelay <info@vi0lation.de>
2016-06-19 14:42:34 +0200

    License information

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -1,7 +1,14 @@
 <?php
-/**
+
+/*
  * This file is part of drdelay/dreamace-voter.
  *
+ * (c) DrDelay <info@vi0lation.de>
+ *
+ * This source file is subject to the MIT license that is bundled with this source code in the file LICENSE.
+ */
+
+/**
  * @author DrDelay <info@vi0lation.de>
  */
 
